3cbfea1c9c0f3a0bc6b7ac51200469f57aa36204
Elif <elifnurbirer1994@gmail.com>
2022-12-27 08:28:47 -0800

    This is ninth commit.

---
+++ src/main/java/org/Day09/Question9.java
@@ -0,0 +1,16 @@
+package org.Day09;
+
+public class Question9 {
+    /**int r;
+    Question7 (int r ) {
+        this.r=r;
+
+    }
+}
+
+class DVD (int r, int c) {
+    int c;
+    DVD (int r, int c){
+
+    }*/
+}
