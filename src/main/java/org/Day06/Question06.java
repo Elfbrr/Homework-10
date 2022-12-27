fd74b1e4cef73fa7e7b9fbdb37da5403efd818b5
Elif <elifnurbirer1994@gmail.com>
2022-12-27 02:03:55 -0800

    This is sixth commit.

---
+++ src/main/java/org/Day06/Question06.java
@@ -0,0 +1,21 @@
+package org.Day06;
+
+import java.io.IOException;
+
+class Question06 {
+    /**public void printFileContent (){
+        //
+        throw new IOException();
+
+    }
+}
+
+    public class Test {
+    public static void main(String[] args) {
+        Question06 xobj = new Question06();
+             xobj.printFileContent();
+
+    }
+     */
+}
+    //Output ==> Option A and Option E
