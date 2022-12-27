e2bfb56fe1be06e4f2b9ec37e66474c5a5c145c2
Elif <elifnurbirer1994@gmail.com>
2022-12-27 10:44:52 -0800

    This is ninth commit.

--- src/main/java/org/Day09/Question9.java
+++ src/main/java/org/Day09/Question9.java
@@ -1,16 +1,31 @@
 package org.Day09;
 
-public class Question9 {
-    /**int r;
-    Question7 (int r ) {
-        this.r=r;
+import java.sql.SQLOutput;
 
+public class Question9 {
+    int r;
+    Question9(int r) {
+        this.r = r;
     }
 }
-
-class DVD (int r, int c) {
+class DVD extends Question9 {
     int c;
-    DVD (int r, int c){
 
-    }*/
+    DVD(int r, int c) {
+      super (r);
+      this.c = c;
+        System.out.println(r + " " + c);
+
+    }
+    public static void main(String[] args) {
+        DVD dvd = new DVD(10,20);
+    }
+
+    /**Output ===> C)super (r);
+                    this.c = c;
+     */
+
 }
+
+
+
